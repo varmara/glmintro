f4b26562d79935cda800b7715b3aae5fea9fbba8
varmara <marina.nikolaeva@gmail.com>
2017-09-07 14:08:07 +0300

    datasets and updated power

---
+++ export_datasets_to_xlsx.R
@@ -0,0 +1,48 @@
+library(XLConnect)
+
+data("Puromycin")
+writeWorksheetToFile("data/Puromycin.xlsx", data = Puromycin, sheet = "Puromycin")
+
+library(boot)
+data("remission")
+writeWorksheetToFile("data/remission.xlsx", data = remission, sheet = "remission")
+data("urine")
+writeWorksheetToFile("data/urine.xlsx", data = urine, sheet = "urine")
+data("catsM")
+writeWorksheetToFile("data/catsM.xlsx", data = catsM, sheet = "catsM")
+data("calcium")
+writeWorksheetToFile("data/calcium.xlsx", data = calcium, sheet = "calcium")
+data("downs.bc")
+writeWorksheetToFile("data/downs.bc.xlsx", data = downs.bc, sheet = "downs.bc")
+data("nitrofen")
+writeWorksheetToFile("data/nitrofen.xlsx", data = nitrofen, sheet = "nitrofen")
+data("dogs")
+writeWorksheetToFile("data/dogs.xlsx", data = dogs, sheet = "dogs")
+
+library(MASS)
+data("Cushings")
+writeWorksheetToFile("data/Cushings.xlsx", data = Cushings, sheet = "Cushings")
+data("cats")
+writeWorksheetToFile("data/cats.xlsx", data = cats, sheet = "cats")
+data("leuk")
+writeWorksheetToFile("data/leuk.xlsx", data = leuk, sheet = "leuk")
+
+library("robustbase")
+data("phosphor")
+writeWorksheetToFile("data/phosphor.xlsx", data = phosphor, sheet = "phosphor")
+data("toxicity")
+writeWorksheetToFile("data/toxicity.xlsx", data = toxicity, sheet = "toxicity")
+data("CrohnD")
+writeWorksheetToFile("data/CrohnD.xlsx", data = CrohnD, sheet = "CrohnD")
+
+library("lme4")
+data("sleepstudy")
+writeWorksheetToFile("data/sleepstudy.xlsx", data = sleepstudy, sheet = "sleepstudy")
+
+# прогестерон
+# мутации
+# goats
+# Loyn
+# abalone
+
+
